294379e19c87c70f9faa1727a355d0b3429f6105
Milekhin <sdgright@yandex.ru>
2026-04-20 17:54:43 +0400

    add stats calculation for numeric column type

--- summarizer/summarizer.py
+++ summarizer/summarizer.py
@@ -12,7 +12,7 @@ class Summarizer:
         self.out_filename = out_filename
         self.stats_df: pd.DataFrame | None = None
 
-    def get_column_category(self, series: pd.Series):
+    def get_column_category(self, series: pd.Series) -> str:
         dtype = series.dtype
         dtype_name = dtype.name
 
@@ -26,3 +26,19 @@ class Summarizer:
             return "datetime"
         
         return "categorical"
+    
+    def calculate_column_stats(self, column_name: str, series: pd.Series):
+        dtype = self.get_column_category(series)
+        stats = {"Column": column_name, "Type": dtype}
+
+        if dtype == "numeric":
+            stats["Min"] = series.min()
+            stats["Max"] = series.max()
+            stats["Mean"] = series.mean()
+            stats["Median"] = series.median()
+            stats["Mode"] = series.mode()
+            stats["Zero rows(%)"] = (series == 0).mean() * 100
+            stats["Variance"] = series.var()
+            stats["Std"] = series.std()
+            stats["IQR"] = series.quantile(0.75) - series.quantile(0.25)
+            stats["CV"] = (stats["std"] / abs(stats["mean"]))
